4aa33dcfc16748a8be0d31b457c122eb2e1a6a9f
Luke Stainer <luke.stainer@oneteam.services>
2024-12-04 00:25:03 +1030

    day2 part 1 done

--- day2.py
+++ day2.py
@@ -1,12 +1,27 @@
-from aocd import data, submit
+txt = open("day2-input.txt").read().split("\n")
 
-answer = 0 
+arrays = []
+for line in txt: 
+  temp = [int(item) for item in line.split(" ")]
+  arrays.append(temp)
 
-# Monotonic strictly increasing stack
-increasingStack = [] 
+safe_reports = 0
+for array in arrays: 
+  # Monotonic strictly increasing stack
+  increasing_stack = [array[0]] 
 
+  # Monotonic strictly decreasing stack
+  decreasing_stack = [array[0]]
+  for i in range(len(array)): 
+    # Add to strictly increasing stack
+    if array[i] > increasing_stack[-1] and array[i] - increasing_stack[-1] <= 3: 
+      increasing_stack.append(array[i])
+    # Add to strictly decreasing stack
+    if array[i] < decreasing_stack[-1] and abs(array[i] - decreasing_stack[-1]) <= 3: 
+      decreasing_stack.append(array[i])
+  
+  if len(increasing_stack) == len(array) or len(decreasing_stack) == len(array): 
+    safe_reports += 1
 
-# Monotonic strictly decreasing stack
-decreasingStack = []
-print(data)
-# submit(answer, part="a", day=2, year=2024)
+print(safe_reports)
+    
